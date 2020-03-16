91792b0c106ee02e2e0a796b8416e1cb58a96020
19521242bao <58712033+19521242bao@users.noreply.github.com>
2020-03-16 19:48:43 +0700

    Create script.js

---
+++ C4E-upload/script.js
@@ -0,0 +1,58 @@
+function calculation() {
+    document.calc.output.value = eval(document.calc.output.value);
+    
+}
+
+function xoa() {
+	document.calc.output.value = '';
+}
+
+// variable s contains the actual value of the button pressed 
+function nhap(s) {
+	document.calc.output.value+=s;
+}
+function abs1(a){
+	return Math.abs(a);
+}
+function absfunction(){
+	let x=document.calc.output.value;
+	document.calc.output.value =eval("Math.abs(x)");
+}
+function tinhGiaiThua(x){
+	let s=1;
+	for(let i=1;i<=x;i++)
+		s*=i;
+	return s;
+}
+function giaithua(){
+	let x=document.calc.output.value;
+	document.calc.output.value=eval('tinhGiaiThua(x)');
+}
+function pow()
+{
+	let x=document.calc.output.value;
+	document.calc.output.value=eval("Math.pow(x,2)");
+}
+function sqrt()
+{
+	let x=document.calc.output.value;
+	document.calc.output.value=eval("Math.sqrt(x)");
+}
+function sin()
+{
+	let x=document.calc.output.value;
+	document.calc.output.value=eval("Math.sin(x)");
+}
+function cos()
+{
+	let x=document.calc.output.value;
+	document.calc.output.value=eval("Math.cos(x)");
+}
+function tan(){
+	let x=document.calc.output.value;
+	document.calc.output.value=eval("Math.tan(x)");
+}
+function log(){
+	let x=document.calc.output.value;
+	document.calc.output.value=eval("Math.log(x)");
+}
